e5d6551557c2980d5ac9bcb7730623979af9f6cd
Joppe Koers <joppe@joppekoers.nl>
2023-03-26 19:19:43 +0200

    v4

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 
 ;(async () => {
 	while (1) {
-		console.log('Hello World! v3')
+		console.log('Hello World! v4')
 		await sleep(1000)
 	}
 })()
